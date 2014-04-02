a8a33c7aa703c53029b45c4a16ca7ff9b53c929b
jeremy clemans & Mark Lehman <student@epicodus.com>
2014-04-02 11:35:35 -0700

    add NEXT lesson functionality

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,14 @@ class Lesson < ActiveRecord::Base
   validates :name, :number, :presence => true, :uniqueness => true
   validates_numericality_of :number
   belongs_to :section
+
+  def next
+    num = 1
+    found_num = nil
+    until found_num != nil || num == Lesson.all.last.number
+      found_num = Lesson.find_by(:number => (self.number.to_i + num).to_s)
+      num += 1
+    end
+    found_num
+  end
 end
--- spec/lesson_spec.rb
+++ spec/lesson_spec.rb
@@ -7,4 +7,12 @@ describe Lesson do
   it { should validate_presence_of :number }
   it { should validate_numericality_of :number}
   it { should belong_to :section }
+
+  context '#next' do
+    it 'returns the lesson with the next highest number from the current lesson.' do
+      current_lesson = Lesson.create({name: 'lesson1', number: 1})
+      next_lesson = Lesson.create({name: 'lesson3', number: 3})
+      current_lesson.next.should eq  next_lesson
+    end
+  end
 end
